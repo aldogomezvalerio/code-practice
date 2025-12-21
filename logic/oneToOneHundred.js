ba2a0618ae303928f4833f61fd9bf3849a1c41f5
aldogomezvalerio <aldogomezvalerio@gmail.com>
2025-12-21 00:21:06 -0400

    Ceate a loop to print from one to 100

---
+++ logic/oneToOneHundred.js
@@ -0,0 +1,4 @@
+//Create a loop from one to 100
+for (var i = 1; i <= 100; i++) {
+    console.log(i);
+}
---
+++ logic/oneToOneHundred.ts
@@ -0,0 +1,4 @@
+//Create a loop from one to 100
+for (let i: number = 1; i <= 100; i++) {
+  console.log(i);
+}
