875036c74c388a0cd124ba036a50818b71af013f
aldogomezvalerio <aldogomezvalerio@gmail.com>
2025-12-26 23:14:27 -0400

    Finished the sort array logic.

--- logic/sortArray.js
+++ logic/sortArray.js
@@ -1,13 +1,14 @@
 var myArray = [5, 3, 8, 1, 2];
 var tempArray = [];
 var minNum = 0;
-for (var i = 0; i < myArray.length; i++) {
+var arrayLength = myArray.length;
+for (var i = 0; i < arrayLength; i++) {
     minNum = myArray[i];
-    for (var j = i + 1; j < myArray.length; j++) {
-        if (myArray[j] < minNum) {
-            minNum = myArray[j];
-        }
+    for (var j = 0; j < myArray.length; j++) {
+        minNum = minNum < myArray[j] ? minNum : myArray[j];
     }
     tempArray.push(minNum);
+    myArray.splice(myArray.indexOf(minNum), 1);
+    minNum = 0;
 }
 console.log(tempArray);
--- logic/sortArray.ts
+++ logic/sortArray.ts
@@ -1,15 +1,16 @@
 let myArray = [5, 3, 8, 1, 2];
 let tempArray = [];
 let minNum: number = 0;
+let arrayLength: number = myArray.length;
 
-for (let i = 0; i < myArray.length; i++) {
+for (let i = 0; i < arrayLength; i++) {
   minNum = myArray[i];
-  for (let j = i + 1; j < myArray.length; j++) {
-    if (myArray[j] < minNum) {
-      minNum = myArray[j];
-    }
-    tempArray.push(minNum);
+  for (let j = 0; j < myArray.length; j++) {
+    minNum = minNum < myArray[j] ? minNum : myArray[j];
   }
+    tempArray.push(minNum);
+    myArray.splice(myArray.indexOf(minNum), 1);
+    minNum = 0; 
   
 }
 
