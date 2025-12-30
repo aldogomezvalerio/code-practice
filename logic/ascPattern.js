73190cefd0fbb31a727b874ede4762e48768d584
aldogomezvalerio <aldogomezvalerio@gmail.com>
2025-12-29 23:17:23 -0400

    Fix the pattern counter in the ascPattern.ts

--- logic/ascPattern.js
+++ logic/ascPattern.js
@@ -3,7 +3,7 @@ var cont = 1;
 console.log("Ascendent Pattern with for loop::");
 for (var i = 0; i < 20; i++) {
     console.log("".concat(cont + i));
-    cont = cont + 1;
+    cont = cont + cont;
 }
 console.log("Ascendent Pattern with while loop::");
 cont = 1;
@@ -11,13 +11,13 @@ var plus = 1;
 while (cont <= 20) {
     console.log("".concat(cont + plus));
     plus = plus + 1;
-    cont++;
+    cont = cont + cont;
 }
-console.log("Ascendent patterntn with do while loop::");
+console.log("Ascendent patterntn with do while loop:");
 cont = 1;
 plus = 1;
 do {
     console.log("".concat(cont + plus));
-    plus = plus + 1;
+    plus = plus + plus;
     cont++;
 } while (cont <= 20);
--- logic/ascPattern.ts
+++ logic/ascPattern.ts
@@ -5,7 +5,7 @@ let cont: number = 1;
 console.log("Ascendent Pattern with for loop::");
 for (let i = 0; i < 20; i++) {
   console.log(`${cont + i}`);
-  cont = cont + 1;
+  cont = cont + cont;
 }
 
 console.log("Ascendent Pattern with while loop::");
@@ -14,15 +14,15 @@ let plus: number = 1;
 while (cont <= 20) {
   console.log(`${cont + plus}`);
   plus = plus + 1;
-  cont++;
+  cont = cont + cont;
 }
 
-console.log("Ascendent patterntn with do while loop::");
+console.log("Ascendent patterntn with do while loop:");
 cont = 1;
 plus = 1;
 do {
   console.log(`${cont + plus}`);
-  plus = plus + 1;
+  plus = plus + plus;
   cont++;
 } while (cont <= 20);
 
