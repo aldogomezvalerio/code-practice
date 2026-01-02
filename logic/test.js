86a8060605644d0eaf7f0699d0d9c5f6a6623315
aldogomezvalerio <aldogomezvalerio@gmail.com>
2026-01-01 20:31:23 -0400

    Finished the logic for the pass generator.

--- logic/test.js
+++ logic/test.js
@@ -1,26 +1,24 @@
 const passRegExpr =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 let pass = "";
+const specialChars = ["@", "$", "!", "%", "*", "?", "&"];
+let cont = 0;
 
 patternArray = [
   () => Math.floor(Math.random() * (122 - 97 + 1)) + 97,
   () => Math.floor(Math.random() * (90 - 65 + 1)) + 65,
   () => Math.floor(Math.random() * (57 - 48 + 1)) + 48,
-  () => Math.floor(Math.random() * (64 - 33 + 1)) + 33,
+  () => specialChars[Math.floor(Math.random() * 6)],
 ];
 
-let cont = 0;
+
 for (let i = 0; i < 15; i++) {
   pass += String.fromCharCode(patternArray[Math.floor(cont)]());
   cont = cont < 3 ? cont + 1 : 0;
 }
 
-console.log(pass, passRegExpr.test(pass));
+pass += specialChars[Math.floor(Math.random() * 6)];
 
-/*while (!passRegExpr.test(pass)) {
-  for (let i = 0; i < 4; i++) {
-    pass += String.fromCharCode(patternArray[i]());
-  }
-  pass += String.fromCharCode(patternArray[Math.floor(Math.random() * 4)]());
-}
-console.log(pass);*/
+console.log(pass);
+
+
