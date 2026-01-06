b2f65df20c76c7bca398e9e731728d16bb4dc4be
aldogomezvalerio <aldogomezvalerio@gmail.com>
2026-01-05 20:39:20 -0400

    Developing the tested logic of a password generator now on a .ts file falled passwordGen.ts

---
+++ logic/passwordGen.ts
@@ -0,0 +1,23 @@
+//This code generate a ramdom password 
+import { Color } from "react-bootstrap/esm/types";
+const passRegExpr =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+let pass: string = "";
+const specialChars:string[] = ["@", "$", "!", "%", "*", "?", "&"];
+let cont: number = 0;
+
+const patternArray: (() => number)[] = [
+  () => Math.floor(Math.random() * (122 - 97 + 1)) + 97,
+  () => Math.floor(Math.random() * (90 - 65 + 1)) + 65,
+  () => Math.floor(Math.random() * (57 - 48 + 1)) + 48,
+];
+
+
+for (let i = 0; i < 15; i++) {
+  pass += String.fromCharCode(patternArray[Math.floor(cont)]());
+  cont = cont < 3 ? cont + 1 : 0;
+}
+
+pass += specialChars[Math.floor(Math.random() * 6)];
+
+console.log(pass);
