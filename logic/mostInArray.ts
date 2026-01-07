e1eb2a63d24c8f01a80c3e85f17cbe1e1ac1c06b
aldogomezvalerio <aldogomezvalerio@gmail.com>
2026-01-07 10:16:36 -0400

    testing a new logic to finding the max common element into an array

---
+++ logic/mostInArray.ts
@@ -0,0 +1,6 @@
+export function mostInArray(arr:any[]): any | null {
+    if (arr.length === 0) return null;
+
+    const frequency : any = {};
+    arr.find
+}
--- logic/test.js
+++ logic/test.js
@@ -1,24 +1,3 @@
-const passRegExpr =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-let pass = "";
-const specialChars = ["@", "$", "!", "%", "*", "?", "&"];
-let cont = 0;
-
-patternArray = [
-  () => Math.floor(Math.random() * (122 - 97 + 1)) + 97,
-  () => Math.floor(Math.random() * (90 - 65 + 1)) + 65,
-  () => Math.floor(Math.random() * (57 - 48 + 1)) + 48,
-  () => specialChars[Math.floor(Math.random() * 6)],
-];
-
-
-for (let i = 0; i < 15; i++) {
-  pass += String.fromCharCode(patternArray[Math.floor(cont)]());
-  cont = cont < 3 ? cont + 1 : 0;
+function MostInArray(arr = []) {
+  if (arr.length === 0) return null;
 }
-
-pass += specialChars[Math.floor(Math.random() * 6)];
-
-console.log(pass);
-
-
