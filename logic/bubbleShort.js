43b7d3782d1612ec9d4ab2de5e69787d18e0e272
aldogomezvalerio <aldogomezvalerio@gmail.com>
2026-01-16 23:48:20 -0400

    Compilation of the bubbleShort.ts into a .js file and testing the code

--- logic/bubbleShort.js
+++ logic/bubbleShort.js
@@ -1,29 +1,19 @@
 "use strict";
 exports.__esModule = true;
-exports.BubblerShort = void 0;
+exports.bubbleSort = void 0;
 //This code is about applying the bubble short argorithem on a list in Typescript
 var myArray = [6, 3, 4, 5, 2, 7, 1];
-//             3, 6, 4, 5, 2, 7, 1
-//             3, 4, 6, 5, 2, 7, 1
-//             3, 4, 5, 6, 2, 7, 1
-//             3, 4, 5, 2, 6, 7, 1
-//             3, 4, 5, 2, 6, 1, 7
-//             3, 4, 5, 2, 1, 6, 7
-//             3, 4, 2, 5, 1, 6, 7
-function BubblerShort(numArray) {
-    var result = [];
-    var n = numArray.length;
-    var tempArray = [];
+function bubbleSort(arr) {
+    var _a;
+    var n = arr.length;
     for (var i = 0; i < n - 1; i++) {
         for (var j = 0; j < n - i - 1; j++) {
-            if (numArray[j] > numArray[j + 1]) {
-                numArray[j] = numArray[j + 1];
-                numArray[j + 1] = numArray[j];
+            if (arr[j] > arr[j + 1]) {
+                _a = [arr[j + 1], arr[j]], arr[j] = _a[0], arr[j + 1] = _a[1];
             }
         }
-        console.log(numArray);
     }
-    return result;
+    return arr;
 }
-exports.BubblerShort = BubblerShort;
-console.log(BubblerShort(myArray));
+exports.bubbleSort = bubbleSort;
+console.log(bubbleSort(myArray));
